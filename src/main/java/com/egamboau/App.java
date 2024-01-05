e99c73732804279e5185469adef9a032cd1580dc
Eduardo Gamboa <egamboau@gmail.com>
2024-01-05 10:18:34 -0600

    Adding logic for antialiasing, adding logic to zoom in and out image with + and - keys

--- src/main/java/com/egamboau/App.java
+++ src/main/java/com/egamboau/App.java
@@ -6,10 +6,13 @@ import com.egamboau.redering.Camera;
 import com.egamboau.utils.Vector3D;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 /**
@@ -17,12 +20,16 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
+    int gcScaleX = 1;
+    int gcScaleY = 1;
+
     @Override
     public void start(Stage primaryStage) {
 
         Camera camera = new Camera();
         camera.setAspectRatio(16.0/9.0);
         camera.setImageWidth(400);
+        camera.setSamplesPerPixel(100);
 
         HittableList world = new HittableList();
         world.add(new Sphere(new Vector3D(0,0,-1), 0.5));
@@ -32,8 +39,36 @@ public class App extends Application {
         Group root = new Group();
         Canvas canvas = new Canvas(camera.getImageWidth(), camera.getImageHeigth());
         GraphicsContext gc = canvas.getGraphicsContext2D();
+        
         root.getChildren().add(canvas);
-        primaryStage.setScene(new Scene(root, camera.getImageWidth(), camera.getImageHeigth()));
+        Scene scene = new Scene(root, camera.getImageWidth(), camera.getImageHeigth());
+        primaryStage.setScene(scene);
+
+
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                // TODO Auto-generated method stub
+                System.out.println(event.getText());
+                System.out.println(canvas.getScaleX());
+                System.out.println(canvas.getScaleY());
+                switch (event.getText()) {
+                    case "+":
+                        canvas.setScaleX(canvas.getScaleX() + 1);
+                        canvas.setScaleY(canvas.getScaleY() + 1);
+                        break;  
+                    case "-":
+                        canvas.setScaleX(canvas.getScaleX() - 1);
+                        canvas.setScaleY(canvas.getScaleY() - 1);        
+                        break;
+                    default:
+                        break;
+                }
+                
+                
+            }
+            
+        });
         primaryStage.show();
 
         
--- src/main/java/com/egamboau/utils/UtilitiesFunctions.java
+++ src/main/java/com/egamboau/utils/UtilitiesFunctions.java
@@ -1,9 +1,20 @@
 package com.egamboau.utils;
 
+import java.util.Random;
+
 public class UtilitiesFunctions {
+
+    private static Random randomGenerator = new Random();
 
     public static double degrees_to_radians(double degrees) {
         return degrees * Math.PI / 180.0;
     }
 
+    public static double getRandomDouble() {
+        return randomGenerator.nextDouble();
+    }
+
+    public static double getRandomDouble(double min, double max) {
+        return min + (max - min) * randomGenerator.nextDouble();
+    }
 }
